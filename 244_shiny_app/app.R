06150e2043327c610971f1457c9d727b692ad906
erindeleonsanchez <erindeleonsanchez@ucsb.edu>
2022-02-06 11:03:30 -0800

    Data summary, Methods, Purpose, Data source

--- 244_shiny_app/app.R
+++ 244_shiny_app/app.R
@@ -25,7 +25,18 @@ ui <- fluidPage(
                               hr(),
                               fluidRow(column(2, verbatimTextOutput("value")))
                               ),
-                 mainPanel("Data")
+                 mainPanel(
+                 "Data summary: \nIn order to examine temporal and spatial patterns of temperature in giant kelp forests, Santa Barbara Long Term Ecological Research (SBC-LTER) has continuously measured ambient sea water temperature at nine reef sites located along the mainland coast of the Santa Barbara Channel and two sites on the North side of Santa Cruz Island since 2000. \n
+                 
+                 Methods for data collection: Submersible temperature loggers (Tidbit, Onset Computer Corporation, Bourne MA) were used to measure benthic temperature at nine coastal reef and two island sites in the Santa Barbara Channel, CA. 
+                 At each site, two temperature loggers were secured to the seafloor at a depth of 7 m (MLLW). Each logger was programmed to record ambient temperature at an interval of 30 minutes. Note that the two loggers were staggered by 15 minutes so the final records reflect a temperature measurement every 15 minutes. The loggers were retrieved and replaced bi-annually, in the early summer and early winter. 
+                 
+                 Purpose of the app:
+                 Giant kelp forests are home to a wide range of organisms, however, abiotic factors, such as temperature, may affect species populations and biological processes on a scale of hours to years. Furthermore, marine heatwaves (MHWs) are predicted to increase drastically in frequency, duration, range, and intensity due to anthropogenic climate change. Studies have shown that MHWs can have vast consequences, including impacts on species abundances, biogeographic range shifts, physiology, and reduced fisheries landings. In order to help local researchers and stakeholders understand temporal and spatial patterns of oceanic temperatures, we have created this app for easy SBC-LTER temperature data visualization.   
+                 
+                 Data source: 
+                 Reed, D., Miller, R. SBC LTER: Reef: Bottom Temperature: Continuous water temperature, ongoing since 2000 ver 26. Environmental Data Initiative. https://doi.org/10.6073/pasta/22ed009da1cf41cbf76490ab2c0c5949. Accessed 2022-02-06."
+                 )
              ) # end sidebarLayout
     ), #end tabPanel Site Data Summaries
     tabPanel("Comparison of Site Temperature Profiles",
