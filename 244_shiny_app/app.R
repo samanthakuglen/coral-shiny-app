3fca98af55e2272cea473cb94aa68b0abb89ff0a
Samantha Kuglen <samanthakuglen@ucsb.edu>
2023-07-08 18:02:22 -0700

    removed yonder package (due to it being removed from CRAN)

--- 244_shiny_app/app.R
+++ 244_shiny_app/app.R
@@ -4,7 +4,6 @@ library(tidyverse)
 library(bslib)
 library(leaflet)
 library(gghighlight)
-library(yonder)
 library(ggbeeswarm)
 library(plotly)
 
@@ -28,105 +27,137 @@ blue_icon <- makeIcon(
 
 ### Define UI for application 
 ui <- fluidPage(
-  theme = bs_theme(version = 4,
-                   bootswatch = "flatly"),
-  
-#   Homepage Title
-    navbarPage("Historical Ocean Temperature Data in the Santa Barbara Channel",
-    
-#   Widget 1: Site About Info
+  theme = bs_theme(version = 4, bootswatch = "flatly"),
+  navbarPage(
+    "Historical Ocean Temperature Data in the Santa Barbara Channel",
+    # Widget 1: Site About Info
     navbarMenu("About",
-      tabPanel("The App",
-               fluidRow(column(
-                 jumbotron("Welcome!", "This app allows users to visualize benthic ocean temperature data collected at reef sites in the Santa Barbara Channel (SBC) by Santa Barbara Long Term Ecological Research (SBC LTER).",button=FALSE)),
-                 br(),
-                 br(),
+               tabPanel("The App",
+                        fluidRow(
+                          column(
+                            width = 12,
+                            h2("Welcome!"),
+                            p("This app allows users to visualize benthic ocean temperature data collected at reef sites in the Santa Barbara Channel (SBC) by Santa Barbara Long Term Ecological Research (SBC LTER).")
+                          )
+                        ),
+                        fluidRow(
+                          column(
+                            width = 12,
+                            h4(HTML('Want to learn more about how these data were collected? Check out the <a href="https://sbclter.msi.ucsb.edu/data/catalog/package/?package=knb-lter-sbc.13" target="_blank">data repository</a>.'))
+                          )
+                        ),
+                        fluidRow(
+                          column(
+                            width = 12,
+                            img(src = "sbc.jpeg", style = "display:block; margin:auto;", height = 400)
+                          )
+                        ),
+                        fluidRow(
+                          column(
+                            width = 12,
+                            h5(HTML('Code and data used to create this Shiny app are available on <a href="https://github.com/samanthakuglen/esm-244-shiny-app" target="_blank">Github</a>.'))
+                          )
+                        )
                ),
-               fluidRow(column(align="center", 
-                               #imageOutput('home_image',inline = TRUE),
-                               h4(HTML('Want to learn more about how these data were collected? Check out the <a href="https://sbclter.msi.ucsb.edu/data/catalog/package/?package=knb-lter-sbc.13" target="_blank">data repository</a>.'))
-               )),
-               br(),
-               br(),
-               HTML('<center><img src = "sbc.jpeg"></center>'),
-               br(),
-               br(),
-               fluidRow(column(align="center",  
-                               h5(HTML('Code and data used to create this Shiny app are available on <a href="https://github.com/samanthakuglen/esm-244-shiny-app" target="_blank">Github</a>.'))
-              ))),  
-      tabPanel("The Fisheries",
-               h3("Check out the most popular local invertebrate seafood in the Santa Barbara Channel!"),
-               sidebarLayout(
-                 sidebarPanel("",
-                      actionLink("species1", "California Spiny Lobster"),
-                      br(),
-                      actionLink("species2", "Red Sea Urchin"), 
-                      br(), 
-                      actionLink("species3", "Mediterranean Mussel")
-                    ),
-               mainPanel(
-                   imageOutput("display"),
-                   textOutput("information"),
-                   uiOutput("link")
-               ))),
-      tabPanel("The Data",
-               fluidRow(column(
-                 jumbotron("Data used here were subset and wrangled into a daily average data frame. It is important to explore your data prior to analyzing it. One way that this can be accomplished through exploratory data visualization like histograms and box plots. By exploring data this way we are able to make decisions on what type of analysis approaches are appropriate for the data.",button=FALSE)),
-                 br(),
-                 br(),
+               tabPanel("The Fisheries",
+                        h3("Check out the most popular local invertebrate seafood in the Santa Barbara Channel!"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            actionLink("species1", "California Spiny Lobster"),
+                            br(),
+                            actionLink("species2", "Red Sea Urchin"),
+                            br(),
+                            actionLink("species3", "Mediterranean Mussel")
+                          ),
+                          mainPanel(
+                            imageOutput("display"),
+                            textOutput("information"),
+                            uiOutput("link")
+                          )
+                        )
                ),
-               sidebarLayout(
-                 sidebarPanel(
-                   checkboxGroupInput(inputId = "reef_code",
-                                      label = h3("Choose Site(s):"),
-                               choiceValues = c("ABUR", "AHND", "AQUE", "BULL", "CARP", "GOLB", "IVEE", "MOHK", "NAPL", "SCDI", "SCTW"),
-                               choiceNames = c("Arroyo Burro", "Arroyo Hondo", "Arroyo Quemado", "Bulito", "Carpinteria", "Goleta Bay", "Isla Vista", "Mohawk", "Naples", "Santa Cruz Island, Diablo", "Santa Cruz Island, Twin Harbor"),
-                               selected = "ABUR") # end select box input
-                 ), # end sidebar Panel
-                 mainPanel(plotOutput(outputId = "boxplot"),
-                           plotOutput(outputId = "histo_plot"))
-               ) #end sidebar Layout
-               ),# end tab panel
-      tabPanel("The Authors",
-               sidebarLayout(
-                 sidebarPanel(
-                   h2("Who are we?"),
-                   p("We are graduate students at the University of California, Santa Barbara in Advanced Data Analysis (ESM 244)."),
-                   br(),
-                   img(src = "UC_Santa_Barbara_Wordmark_Navy_RGB.png", height = 70, width = 200)
-                 ),
-                 mainPanel(
-                   h3(HTML('<a href= "https://german-sil.github.io/gds/" target="_blank">Germán Silva</a>')),
-                   img(src = "german.jpg", height = 200, width = 150),
-                   br(),
-                   br(),
-                   h3(HTML('<a href= "https://samanthakuglen.github.io/personal-website/" target="_blank">Samantha Kuglen</a>')),
-                   img(src = "sam.jpg", height = 130, width = 150),
-                   br(),
-                   br(),
-                   h3(HTML('<a href= "https://erindeleonsanchez.github.io/ESM-244-Website/" target="_blank">Erin de Leon Sanchez</a>')),
-                   img(src = "erin.jpeg", height = 150, width = 150),
-                   br(),
-                   br(),
-                   br(),
-                 )
-               )),
-             ),
-    
-#   Widget 2: Map of Sites 
+               tabPanel("The Data",
+                        fluidRow(
+                          column(
+                            width = 12,
+                            h2("Data Subset and Wrangling"),
+                            p("Data used here were subset and wrangled into a daily average data frame. It is important to explore your data prior to analyzing it. One way that this can be accomplished is through exploratory data visualization like histograms and box plots. By exploring data this way, we can make decisions on what type of analysis approaches are appropriate for the data.")
+                          )
+                        ),
+                        sidebarLayout(
+                          sidebarPanel(
+                            checkboxGroupInput(
+                              inputId = "reef_code",
+                              label = h3("Choose Site(s):"),
+                              choiceValues = c(
+                                "ABUR", "AHND", "AQUE", "BULL", "CARP",
+                                "GOLB", "IVEE", "MOHK", "NAPL", "SCDI", "SCTW"
+                              ),
+                              choiceNames = c(
+                                "Arroyo Burro", "Arroyo Hondo", "Arroyo Quemado", "Bulito", "Carpinteria",
+                                "Goleta Bay", "Isla Vista", "Mohawk", "Naples", "Santa Cruz Island, Diablo", "Santa Cruz Island, Twin Harbor"
+                              ),
+                              selected = "ABUR"
+                            )
+                          ),
+                          mainPanel(
+                            plotOutput(outputId = "boxplot"),
+                            plotOutput(outputId = "histo_plot")
+                          )
+                        )
+               ),
+               tabPanel("The Authors",
+                        sidebarLayout(
+                          sidebarPanel(
+                            h2("Who are we?"),
+                            p("We are graduate students at the University of California, Santa Barbara in Advanced Data Analysis (ESM 244)."),
+                            br(),
+                            img(
+                              src = "UC_Santa_Barbara_Wordmark_Navy_RGB.png",
+                              height = 70, width = 200
+                            )
+                          ),
+                          mainPanel(
+                            h3(HTML('<a href= "https://german-sil.github.io/gds/" target="_blank">Germán Silva</a>')),
+                            img(src = "german.jpg", height = 200, width = 150),
+                            br(),
+                            br(),
+                            h3(HTML('<a href= "https://samanthakuglen.github.io/personal-website/" target="_blank">Samantha Kuglen</a>')),
+                            img(src = "sam.jpg", height = 130, width = 150),
+                            br(),
+                            br(),
+                            h3(HTML('<a href= "https://erindeleonsanchez.github.io/ESM-244-Website/" target="_blank">Erin de Leon Sanchez</a>')),
+                            img(src = "erin.jpeg", height = 150, width = 150),
+                            br(),
+                            br(),
+                            br()
+                          )
+                        )
+               )
+    ),
+    # Widget 2: Map of Sites
     tabPanel("Map of Sites",
              sidebarLayout(
-                 sidebarPanel(radioButtons(inputId = "site_name",
-                                           label = "Select a site to highlight:",
-                                           choiceValues = c("ABUR", "AHND", "AQUE", "BULL", "CARP", "GOLB", "IVEE", "MOHK", "NAPL", "SCDI", "SCTW"),
-                                           choiceNames = c("Arroyo Burro", "Arroyo Hondo", "Arroyo Quemado", "Bulito", "Carpinteria", "Goleta Bay", "Isla Vista", "Mohawk", "Naples", "Santa Cruz Island, Diablo", "Santa Cruz Island, Twin Harbor")
-                                           )
-                              ),
-                 mainPanel(leafletOutput(outputId = "site_map"),
-                           textOutput("site_description"))
-             ) # end sidebarLayout
-             ), #end tabPanel Map of Sites
-    
+               sidebarPanel(
+                 radioButtons(
+                   inputId = "site_name",
+                   label = "Select a site to highlight:",
+                   choiceValues = c(
+                     "ABUR", "AHND", "AQUE", "BULL", "CARP",
+                     "GOLB", "IVEE", "MOHK", "NAPL", "SCDI", "SCTW"
+                   ),
+                   choiceNames = c(
+                     "Arroyo Burro", "Arroyo Hondo", "Arroyo Quemado", "Bulito", "Carpinteria",
+                     "Goleta Bay", "Isla Vista", "Mohawk", "Naples", "Santa Cruz Island, Diablo", "Santa Cruz Island, Twin Harbor"
+                   )
+                 )
+               ),
+               mainPanel(
+                 leafletOutput(outputId = "site_map"),
+                 textOutput("site_description")
+               )
+             )
+    ),
 #   Widget 3: Temperature Time Series
     tabPanel("Comparison of Site Temperature Profiles",
              sidebarLayout(
